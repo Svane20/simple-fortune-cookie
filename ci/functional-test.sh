5339fb457a2545ab6e296d79d4abd77ac25e346d
Sebastian <sechr17@student.sdu.dk>
2024-08-15 14:30:35 +0200

    testing variabels

--- ci/functional-test.sh
+++ ci/functional-test.sh
@@ -2,6 +2,9 @@
 node_ip = NODE_EXTERNAL_IP=$(kubectl --kubeconfig kubeconfig get nodes -o jsonpath='{.items[0].status.addresses[?(@.type=="ExternalIP")].address}')
 frontend_port = $(kubectl --kubeconfig kubeconfig get service frontend -o jsonpath='{.spec.ports[0].nodePort}')
 
+echo "nodeip: $node_ip"
+echo "port: $frontend_port"
+
 frontend_url = "http://$node_ip:$frontend_port"
 
 echo "url: $frontend_url"
