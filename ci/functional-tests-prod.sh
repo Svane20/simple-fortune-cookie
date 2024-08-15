daf0b79e093c3a9be93d005fcb84b06bda4b9119
ks <svane.kasper20@gmail.com>
2024-08-15 15:11:18 +0200

    task/complete Working on final steps

--- ci/functional-tests-prod.sh
+++ ci/functional-tests-prod.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Extract the host using awk
-host=$(kubectl get ingress frontend-ingress -o jsonpath='{.spec.rules[0].host}')
+host=$(kubectl --kubeconfig kubeconfig get ingress frontend-ingress -o jsonpath='{.spec.rules[0].host}')
 
 echo "Extracted host: $host"
