78cc3bdd4da77dfb3727e44e9e15fbe05d092b63
ks <svane.kasper20@gmail.com>
2024-08-15 15:13:39 +0200

    task/complete Working on final steps

--- ci/functional-tests-prod.sh
+++ ci/functional-tests-prod.sh
@@ -2,5 +2,30 @@
 
 # Extract the host using awk
 host=$(kubectl --kubeconfig kubeconfig get ingress frontend-ingress -o jsonpath='{.spec.rules[0].host}')
+frontend_url="https://$host"
 
-echo "Extracted host: $host"
+echo "url: $frontend_url"
+
+echo "Testing frontend availability..."
+if curl -s --head --request GET $frontend_url | grep "200 OK" > /dev/null; then
+    echo "Frontend is reachable."
+else
+    echo "Error: Frontend is not reachable!"
+    exit 1
+fi
+
+echo "Testing all endpoint..."
+if curl -s --head --request GET $frontend_url/api/all | grep "200 OK" > /dev/null; then
+    echo "Can fetch all cookies"
+else
+    echo "Error: Can't fetch cookies!"
+    exit 1
+fi
+
+echo "Testing random endpoint..."
+if curl -s --head --request GET $frontend_url/api/random | grep "200 OK" > /dev/null; then
+    echo "Can fetch random cookies"
+else
+    echo "Error: Can't fetch random cookies"
+    exit 1
+fi
